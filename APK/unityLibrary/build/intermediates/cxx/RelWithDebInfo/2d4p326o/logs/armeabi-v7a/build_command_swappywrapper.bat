25c9c037510599af18ca47e224cb571e77b768dd
haboston90-lang <haboston90@gmail.com>
2026-06-12 13:48:36 +0200

    More Updates

--- APK/unityLibrary/build/intermediates/cxx/RelWithDebInfo/2d4p326o/logs/armeabi-v7a/build_command_swappywrapper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.63f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\Downloads\\Ndimthandile_Mkalipi_08HA2600763_ HFGE1181_SA1\\APK\\unityLibrary\\.cxx\\RelWithDebInfo\\2d4p326o\\armeabi-v7a" ^
-  swappywrapper
